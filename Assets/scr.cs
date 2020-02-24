cbdcfdb6d06b301e917f10d267f1cab3523e7bb3
William Lee <52926429+SSonagi@users.noreply.github.com>
2020-02-24 01:51:49 -0500

    Fixed Camera

---
+++ Assets/scr.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr : MonoBehaviour
+{
+    public Transform targetTransform;
+    public float y;
+    Vector3 tempVec3 = new Vector3();
+
+    void LateUpdate()
+    {
+        tempVec3.x = targetTransform.position.x;
+        tempVec3.y = y;
+        tempVec3.z = 10;
+        this.transform.position = tempVec3;
+    }
+}
